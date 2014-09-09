88e4fa9f52c7586ce48ba177406682504f68bdb4
NajiEnsi <alazhar.naji@gmail.com>
2014-09-09 16:42:56 +0200

    Optimal Weights

--- WpfApplication1/WpfApplication1/Services/Optimization.cs
+++ WpfApplication1/WpfApplication1/Services/Optimization.cs
@@ -20,6 +20,24 @@ namespace WpfApplication1.Services
             ref int info
         );
 
+        [DllImport(pathToDll, EntryPoint = "WREallocIT", CallingConvention = CallingConvention.Cdecl)]
+        public static extern int OptWeights(
+            ref int nbAssets,
+            double[,] cov,
+            double[] expectedReturns,
+            double[] benchmarkCov,
+            ref double benchmarkExpectedReturn,
+            ref int nbEqConst,
+            ref int nbIneqConst,
+            double[,] C,
+            double[] b,
+            double[] minWeights,
+            double[] maxWeights,
+            ref double relativeTargetReturn,
+            double[] optimalWeights,
+            ref int info
+        );
+
         private Measure _measure;
 
         public Optimization()
@@ -28,7 +46,8 @@ namespace WpfApplication1.Services
 
         }
 
-        public double[,] Calculate(double[,] returns){
+        public double[,] computeCovarianceMatrix(double[,] returns)
+        {
             int dataSize = returns.GetLength(0);
             int nbAssets = returns.GetLength(1);
             double[,] covMatrix = new double[nbAssets, nbAssets];
@@ -41,5 +60,52 @@ namespace WpfApplication1.Services
             }
             return covMatrix;
         }
+
+        public double[] computeOptWeights(double[,] covMatrix, double[] expectedReturns, double mu)
+        {
+            int nbAssets = covMatrix.GetLength(0)-1;
+            double[] benchmarkCov= new double[nbAssets];
+            double benchmarkExpectedReturn;
+            int nbEqConst= 1;
+            int nbIneqConst= 0;
+            double[,] C= new double[nbAssets,1];
+            double[] b= new double[nbAssets];
+            double[] minWeights= new double[nbAssets];
+            double[] maxWeights= new double[nbAssets];
+            double[] shareExpectedReturns= new double[nbAssets];
+            double relativeTargetReturn;
+            double[,] cov= new double[nbAssets,nbAssets];
+            
+            for (int i = 0; i < nbAssets; i++ )
+            {
+                maxWeights[i]= 1;
+                minWeights[i]= 0;
+                C[i, 1]= 1;
+                b[i]= 1;
+                benchmarkCov[i] = covMatrix[nbAssets, i];       
+                shareExpectedReturns[i]= expectedReturns[i];
+                for(int j = 0; j < nbAssets; j++ ){
+                    cov[i,j]= covMatrix[i, j];
+                }
+            }
+           
+
+            benchmarkExpectedReturn = expectedReturns.Last();
+            relativeTargetReturn= mu;
+            int info = 0;
+
+            double[] optimalWeights = new double[nbAssets];
+            int returnFromOpt = OptWeights(ref nbAssets, cov, shareExpectedReturns, benchmarkCov, ref benchmarkExpectedReturn, ref nbEqConst, ref nbIneqConst, C, b, minWeights, maxWeights, ref mu, optimalWeights, ref info);
+
+             if (returnFromOpt != 0)
+            {
+
+                throw new Exception(); // Check out what went wrong here
+            }
+            return optimalWeights;
+        }
+
+
+
     }
 }
