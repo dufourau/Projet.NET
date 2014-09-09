fd480abcb12b854a675331b1879dc5961e6e4366
dufourau <duf.aurelien@gmail.com>
2014-09-09 11:06:52 +0200

    Add the project architecture

---
+++ WpfApplication1/WpfApplication1/Services/Optimization.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApplication1.Services
+{
+    internal class Optimization
+    {
+        const string pathToDll = @"Path\To\x64\Or\X86\wre-ensimag-c-4.1.dll";
+
+        [DllImport(pathToDll, EntryPoint = "WREmodelingCov", CallingConvention = CallingConvention.Cdecl)]
+        public static extern int NORMmodelingCov(
+            ref int returnsSize,
+            ref int nbSec,
+            double[,] secReturns,
+            double[,] covMatrix,
+            ref int info
+        );
+
+        private Measure _measure;
+
+        public Optimization()
+        {
+            _measure = Measure.MinStdDev;
+
+        }
+
+        public double[,] Calculate(double[,] returns){
+            int dataSize = returns.GetLength(0);
+            int nbAssets = returns.GetLength(1);
+            double[,] covMatrix = new double[nbAssets, nbAssets];
+            int info = 0;
+            int returnFromNorm = NORMmodelingCov(ref dataSize, ref nbAssets, returns, covMatrix, ref info);
+            if (returnFromNorm != 0)
+            {
+
+                throw new Exception(); // Check out what went wrong here
+            }
+            return covMatrix;
+        }
+    }
+}
